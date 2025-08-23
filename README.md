adcb1c3694d391061c3e87b0f32c518604f4d945
superb1aze <romanabrosimov1@gmail.com>
2025-08-23 19:05:50 +0300

    main and readme changes

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-A repository for a small pet project, aimed at learning the basics of working with FastAPI, SQLAlchemy, and Alembic. Feel free to comment and leave helpful advices on improvements/fixes.
+# Cryptoweb Project
+
+[![Version](https://img.shields.io/badge/version-0.0.0_alpha-blue.svg)]()
+
+A small **pet project** aimed at learning the basics of **FastAPI**, **SQLAlchemy**, and **Alembic**.  
+Feel free to comment and leave helpful advice on improvements or fixes.
+
+---
+
+## Installation
+
+### 1. Clone the repository
+```bash
+git clone https://github.com/SuperB1aze/cryptoweb-project.git
+cd cryptoweb-project
+```
+
+### 2. Create a virtual environment
+
+#### Windows:
+```bash
+python -m venv venv
+```
+
+#### Linux/macOS:
+```bash
+python3 -m venv venv
+```
+
+### 3. Activate the virtual environment
+
+#### Windows (PowerShell):
+```bash
+.\venv\Scripts\activate
+```
+#### Linux/macOS:
+```bash
+source venv/bin/activate
+```
+
+### 4. Install dependencies
+```bash
+pip install -r requirements.txt
+```
+
+### 5. Run the application
+
+#### Windows:
+```bash
+python main.py
+```
+
+#### Linux/macOS:
+```bash
+python3 main.py
+```
--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from fastapi.openapi.utils import get_openapi
 import uvicorn, sys, os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 from src.app.routers import user_routes, post_routes, auth_routes
 
-app = FastAPI()
-app.include_router(user_routes.router_user)
-app.include_router(post_routes.router_post)
-app.include_router(auth_routes.router_auth)
+app = FastAPI(title="Cryptoweb Project", docs_url="/api/v1/docs", redoc_url="/api/v1/redoc", openapi_url="/api/v1/openapi.json")
+
+app_v1 = APIRouter(prefix="/api/v1")
+
+app_v1.include_router(user_routes.router_user)
+app_v1.include_router(post_routes.router_post)
+app_v1.include_router(auth_routes.router_auth)
+
+app.include_router(app_v1)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    
+    openapi_schema = get_openapi(
+        title=app.title,
+        version="0.0.0 alpha",
+        routes=app.routes,
+    )
+    openapi_schema["servers"] = [{"url": "/api/v1"}]
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
