b008b90760687593bea7458940c122b458daa02a
superb1aze <romanabrosimov1@gmail.com>
2025-09-02 06:11:32 +0300

    test db url added

--- src/config.py
+++ src/config.py
@@ -16,22 +16,24 @@ class DB_Settings(BaseSettings):
     DB_MIGRATION_USER: str | None = os.getenv("DB_MIGRATION_USER")
     DB_MIGRATION_PASS: str | None = os.getenv("DB_MIGRATION_PASS")
 
+    TEST_MODE: int | None = int(os.getenv("TEST_MODE"))
     DB_TEST_HOST: str | None = os.getenv("DB_TEST_HOST")
     DB_TEST_USER: str | None = os.getenv("DB_TEST_USER")
     DB_TEST_PASS: str | None = os.getenv("DB_TEST_PASS")
     DB_TEST_PORT: int | None = int(os.getenv("DB_TEST_PORT"))
+    DB_TEST_NAME: str | None = os.getenv("DB_TEST_NAME")
+
+    @property
+    def DATABASE_URL_asyncpg_testing(self):
+        return f"postgresql+asyncpg://{self.DB_TEST_USER}:{self.DB_TEST_PASS}@{self.DB_TEST_HOST}:{self.DB_TEST_PORT}/{self.DB_TEST_NAME}"
 
     @property
     def DATABASE_URL_asyncpg(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}" if self.TEST_MODE != 1 else self.DATABASE_URL_asyncpg_testing
     
     @property
     def DATABASE_URL_asyncpg_migrations(self):
-        return f"postgresql+asyncpg://{self.DB_MIGRATION_USER}:{self.DB_MIGRATION_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-    
-    @property
-    def DATABASE_URL_asyncpg_testing(self):
-        return f"postgresql+asyncpg://{self.DB_TEST_USER}:{self.DB_TEST_PASS}@{self.DB_TEST_HOST}:{self.DB_TEST_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_MIGRATION_USER}:{self.DB_MIGRATION_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}" if self.TEST_MODE != 1 else self.DATABASE_URL_asyncpg_testing
     
 
 BASE_DIR = Path(__file__).resolve().parent.parent
